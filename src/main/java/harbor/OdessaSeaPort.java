85bc63b9188d657bcce8253a2807e5af87d31c76
Sergei Berezhnoi <berezhnoi.serg@gmail.com>
2018-10-28 17:53:58 +0300

    first commit

---
+++ src/main/java/harbor/OdessaSeaPort.java
@@ -0,0 +1,13 @@
+package harbor;
+
+public class OdessaSeaPort {
+
+    public static String sortSumPaymentAsc(AbstractShip[] arrayShips) {
+
+        /*BODY*/
+        return null;
+    }
+
+    public String toPrint(AbstractShip[] arrayShips) {
+    }
+}
---
+++ src/test/java/harbor/OdessaSeaPortTest.java
@@ -0,0 +1,23 @@
+package harbor;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class OdessaSeaPortTest {
+
+
+    @Test
+    public void sortSumPaymentAsc_Test() {
+        OdessaSeaPort port = new OdessaSeaPort();
+        AbstractShip[] arrayShips = new AbstractShip[3];
+
+        arrayShips[0] = new Cargo("TestCargoName", 100, 100, 100, 10);
+        arrayShips[1] = new Tanker("TestNameTatnker", 100, 100, 100, 100);
+        arrayShips[2] = new Liner("TestNameLiner", 100, 100, 100, 300);
+
+        assertEquals("{TestNameTatnker=25000};" + "{TestNameCargo=55000};" +
+                "{TestNameLiner=100000};",port.toPrint());
+    }
+
+}
